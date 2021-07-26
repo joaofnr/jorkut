a3beee61f73b0adfea284076c798f41d41a67e22
João Nunes Rios <joao@pw3.com.br>
2021-07-26 20:44:05 -0300

    logout page

---
+++ pages/logout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import nookies from 'nookies';
+
+export default function LogoutScreen() {
+  nookies.destroy(null, 'USER_TOKEN');
+  return (
+    <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <div className="loginScreen">
+        <section className="logoArea">
+          <img src="https://alurakut.vercel.app/logo.svg" />
+
+          <p><strong>Conecte-se</strong> aos seus amigos e familiares usando recados e mensagens instantâneas</p>
+          <p><strong>Conheça</strong> novas pessoas através de amigos de seus amigos e comunidades</p>
+          <p><strong>Compartilhe</strong> seus vídeos, fotos e paixões em um só lugar</p>
+        </section>
+
+        <section className="formArea">
+          <h2>Você foi desconectado.</h2>
+          <p><a href="/login">Voltar</a></p>
+        </section>
+
+        <footer className="footerArea">
+          <p>
+            © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+          </p>
+        </footer>
+      </div>
+    </main>
+  )
+} 
